da619fb9b9bf26830e30c980a1a9ab18a6640400
Setup Wizard Team <android-setup-team-eng@google.com>
2026-03-19 04:48:49 +0000

    Import updated Android SetupCompat Library 885955286
    Copied from google3/third_party/java_src/android_libs/setupcompat
    
    Test: mm
    
    Included changes:
      - 885955286 [Hide footer] Add IME visibility guard for GlifLayout foo...
      - 878921419 Introduce a flag for disabling transitions between modal ...
      - 878894821 [HideFooter] Add OnVisibilityChangeListener to ButtonBarL...
    
    PiperOrigin-RevId: 885955286
    Change-Id: I0e92e2a8f5a61fc673466f76ff8475a2e1f359bd

--- main/java/com/google/android/setupcompat/view/ButtonBarLayout.java
+++ main/java/com/google/android/setupcompat/view/ButtonBarLayout.java
@@ -21,6 +21,7 @@ import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
+import androidx.annotation.Nullable;
 import com.google.android.setupcompat.R;
 import com.google.android.setupcompat.partnerconfig.PartnerConfigHelper;
 import com.google.android.setupcompat.template.FooterActionButton;
@@ -43,6 +44,7 @@ public class ButtonBarLayout extends LinearLayout {
   private int originalPaddingRight;
 
   private boolean stackedButtonForExpressiveStyle;
+  private OnVisibilityChangeListener onVisibilityChangeListener;
 
   public ButtonBarLayout(Context context) {
     super(context);
@@ -50,6 +52,28 @@ public class ButtonBarLayout extends LinearLayout {
 
   public ButtonBarLayout(Context context, AttributeSet attrs) {
     super(context, attrs);
+  }
+
+  /** Listener for visibility change of the button bar. */
+  public interface OnVisibilityChangeListener {
+    void onVisibilityChanged(int visibility);
+  }
+
+  /** Sets the listener for visibility change of the button bar. */
+  public void setOnVisibilityChangeListener(@Nullable OnVisibilityChangeListener listener) {
+    this.onVisibilityChangeListener = listener;
+  }
+
+  @Override
+  public void setVisibility(int visibility) {
+    if (getVisibility() == visibility) {
+      return;
+    }
+
+    super.setVisibility(visibility);
+    if (onVisibilityChangeListener != null) {
+      onVisibilityChangeListener.onVisibilityChanged(visibility);
+    }
   }
 
   @Override
